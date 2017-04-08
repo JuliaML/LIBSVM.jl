ea871449773820b4022d08ffadc21e5c6239a845
Matti Pastell <matti.pastell@helsinki.fi>
2017-04-08 13:13:59 +0300

    Add build for windows

--- deps/build.jl
+++ deps/build.jl
@@ -2,7 +2,8 @@
 if is_windows()
     lib = joinpath(joinpath(dirname(@__FILE__), "libsvm.dll"))
     if !isfile(lib)
-        #download(, lib)
+        info("Downloading LIBSVM binary")
+        download("https://mpastell.github.io/SVMs.jl/bindeps/libsvm.dll", lib)
     end
 else
     cd(joinpath(dirname(@__FILE__), "libsvm-3.17"))
