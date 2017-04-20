5db8c258d7ecf8e333c4d011a628858ade6d386c
Matti Pastell <matti.pastell@helsinki.fi>
2017-04-20 08:54:39 +0300

    Add 32bit build

--- deps/build.jl
+++ deps/build.jl
@@ -3,7 +3,11 @@ if is_windows()
     lib = joinpath(joinpath(dirname(@__FILE__), "libsvm.dll"))
     if !isfile(lib)
         info("Downloading LIBSVM binary")
-        download("https://mpastell.github.io/LIBSVM.jl/bindeps/libsvm.dll", lib)
+        if Sys.WORD_SIZE == 64
+            download("https://mpastell.github.io/LIBSVM.jl/bindeps/libsvm.dll", lib)
+        else
+            download("https://mpastell.github.io/LIBSVM.jl/bindeps/libsvm32.dll", lib)
+        end
     end
 else
     cd(joinpath(dirname(@__FILE__), "libsvm-3.22"))
