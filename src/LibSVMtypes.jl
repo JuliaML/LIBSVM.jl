35a640bee8f318a3d335cde1a0b4127eb2d77b70
Iblis Lin <iblis@hs.ntnu.edu.tw>
2021-06-10 19:00:08 +0800

    LibSVMtypes: using C style type alias (#74)

--- src/LibSVMtypes.jl
+++ src/LibSVMtypes.jl
@@ -1,49 +1,49 @@
 # https://github.com/cjlin1/libsvm/blob/557d85749aaf0ca83fd229af0f00e4f4cb7be85c/svm.h
 
 struct SVMNode
-    index::Int32
-    value::Float64
+    index::Cint
+    value::Cdouble
 end
 
 struct SVMProblem
-    l::Int32
-    y::Ptr{Float64}
+    l::Cint
+    y::Ptr{Cdouble}
     x::Ptr{Ptr{SVMNode}}
 end
 
 struct SVMParameter
-    svm_type   ::Int32
-    kernel_type::Int32
-    degree     ::Int32
-    gamma      ::Float64
-    coef0      ::Float64
+    svm_type   ::Cint
+    kernel_type::Cint
+    degree     ::Cint
+    gamma      ::Cdouble
+    coef0      ::Cdouble
 
     # for training only */
-    cache_size  ::Float64
-    eps         ::Float64
-    C           ::Float64
-    nr_weight   ::Int32
-    weight_label::Ptr{Int32}
-    weight      ::Ptr{Float64}
-    nu          ::Float64
-    p           ::Float64
-    shrinking   ::Int32
-    probability ::Int32
+    cache_size  ::Cdouble
+    eps         ::Cdouble
+    C           ::Cdouble
+    nr_weight   ::Cint
+    weight_label::Ptr{Cint}
+    weight      ::Ptr{Cdouble}
+    nu          ::Cdouble
+    p           ::Cdouble
+    shrinking   ::Cint
+    probability ::Cint
 end
 
 struct SVMModel
    param     ::SVMParameter
-   nr_class  ::Int32
-   l         ::Int32
+   nr_class  ::Cint
+   l         ::Cint
    SV        ::Ptr{Ptr{SVMNode}}
-   sv_coef   ::Ptr{Ptr{Float64}}
-   rho       ::Ptr{Float64}
-   probA     ::Ptr{Float64}
-   probB     ::Ptr{Float64}
-   sv_indices::Ptr{Int32}
+   sv_coef   ::Ptr{Ptr{Cdouble}}
+   rho       ::Ptr{Cdouble}
+   probA     ::Ptr{Cdouble}
+   probB     ::Ptr{Cdouble}
+   sv_indices::Ptr{Cint}
 
-   label     ::Ptr{Int32}
-   nSV       ::Ptr{Int32}
+   label     ::Ptr{Cint}
+   nSV       ::Ptr{Cint}
 
-   free_sv   ::Int32
+   free_sv   ::Cint
 end
